90f98cef51fbb107ca7a6599b0f1391b102bbbdb
Glenn Tippett <glenn.tippett@gmail.com>
2021-10-09 17:24:43 +1100

    implement banner into main app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import './App.css';
-import Nav from './components/Nav';
+import "./App.css";
+import Nav from "./components/Nav";
+import Banner from "./components/Banner";
+import ButtonPrimary from "./components/Button";
+import Container from "./components/Container";
 
 function App() {
   return (
     <div className="App">
       <Nav />
+      <Banner>
+        <Container>
+          <h3>Showcasing junior developers to recruiters</h3>
+          <p>
+            Enance is a platform to showcase junior developers as well as be a
+            community for developers to discuss and share ideas
+          </p>
+          <ButtonPrimary buttonText="Get Started" />
+        </Container>
+      </Banner>
     </div>
   );
 }
