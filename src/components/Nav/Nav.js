bd8d7915984c94ac9c2dbfdc43fdc4b00ef785b2
Glenn Tippett <glenn.tippett@gmail.com>
2021-12-23 07:54:05 +1100

    make responsive nav

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,106 +1,108 @@
-import React, { useState, useEffect } from "react";
-
 import {
+  Box,
   Flex,
-  Spacer,
-  Text,
   Avatar,
-  useDisclosure,
-  Popover,
-  PopoverTrigger,
-  PopoverContent,
-  PopoverBody,
-  PopoverFooter,
-  PopoverArrow,
-  PopoverCloseButton,
+  HStack,
   IconButton,
+  Button,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  MenuDivider,
+  useDisclosure,
+  useColorModeValue,
+  Stack,
+  Heading,
+  Text
 } from "@chakra-ui/react";
 import { HamburgerIcon, CloseIcon } from "@chakra-ui/icons";
 import { Link } from "react-router-dom";
 import { LoginButton, LogoutButton } from "../../services/Authentication";
 import { useAuth0 } from "@auth0/auth0-react";
-import { NavLinks } from "./NavLinks";
 
-const Nav = () => {
-  const [display, setDisplay] = useState("none");
+const Links = ["About", "Challenges", "Blog"];
+
+const NavLink = ({ children }) => (
+
+  <Link
+    px={2}
+    py={1}
+    rounded={"md"}
+    _hover={{
+      textDecoration: "none",
+      bg: useColorModeValue("gray.200", "gray.700"),
+    }}
+    href={"#"}
+    to={`/${children}`}
+  >
+    {children}
+  </Link>
+);
+
+export default function Simple() {
+  const { isOpen, onOpen, onClose } = useDisclosure();
   const { user, isAuthenticated } = useAuth0();
-  const { onOpen, onClose } = useDisclosure();
-  const btnRef = React.useRef();
-
-  const textHover = {
-    background: "#F7F7F7",
-    cursor: "pointer",
-    borderRadius: "8px",
-  };
-
-  console.log(display)
-
-  useEffect(() => {
-    // ...
-  }, [display]);
 
   return (
-    <Flex
-      align="center"
-      h="64px"
-      pl={16}
-      pr={16}
-      borderBottom="1px"
-      borderColor="gray.200"
-    >
-      <IconButton
-        aria-label="Open Menu"
-        size="md"
-        mr={2}
-        icon={display === 'none' ? <HamburgerIcon /> : <CloseIcon />}
-        display={["flex", "flex", "none", "none"]}
-        onClick={() => display === 'flex' ? setDisplay('none') : setDisplay('flex')}
-      />
-
-      <NavLinks textHoverStyle={textHover} display={display} />
-
-
-      {/* <IconButton
-        mt={2}
-        mr={2}
-        aria-label="Close Menu"
-        size="lg"
-        icon={<CloseIcon />}
-        display={display}
-        onClick={() => setDisplay('none')}
-      /> */}
-
-      <Spacer />
-
-      <Flex align="center">
-        {!isAuthenticated && <LoginButton hover={textHover} />}
-        {isAuthenticated && (
-          <>
-            <Popover>
-              <PopoverTrigger>
-                <Flex align="center" mr={4} ref={btnRef} onClick={onOpen}>
-                  <Text mr={2}>{user.name}</Text>
+    <>
+      <Box px={4}>
+        <Flex h={16} alignItems={"center"} justifyContent={"space-between"}>
+          <IconButton
+            size={"md"}
+            icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
+            aria-label={"Open Menu"}
+            display={{ md: "none" }}
+            onClick={isOpen ? onClose : onOpen}
+          />
+          <HStack spacing={8} alignItems={"center"}>
+            <Heading size="lg" pr={8}>
+              <Link to="/">Enance</Link>
+            </Heading>
+            <HStack
+              as={"nav"}
+              spacing={4}
+              display={{ base: "none", md: "flex" }}
+            >
+              {Links.map((link) => (
+                <NavLink key={link}>{link}</NavLink>
+              ))}
+            </HStack>
+          </HStack>
+          <Flex alignItems={"center"}>
+            {!isAuthenticated && <LoginButton />}
+            {isAuthenticated && (
+              <Menu>
+                <Text mr={2}>{user.name}</Text>
+                <MenuButton
+                  as={Button}
+                  rounded={"full"}
+                  variant={"link"}
+                  cursor={"pointer"}
+                  minW={0}
+                >
                   <Avatar src={user.picture} size="sm" name={user.name} />
-                </Flex>
-              </PopoverTrigger>
+                </MenuButton>
+                <MenuList>
+                  <MenuItem><Link to="/profile">Profile</Link></MenuItem>
+                  <MenuDivider />
+                  <MenuItem><LogoutButton /></MenuItem>
+                </MenuList>
+              </Menu>
+            )}
+          </Flex>
+        </Flex>
 
-              <PopoverContent>
-                <PopoverArrow />
-                <PopoverCloseButton />
-                <PopoverBody>
-                  <Link to="/profile">Profile</Link>
-                  <Text>Something</Text>
-                </PopoverBody>
-                <PopoverFooter>
-                  <LogoutButton hover={textHover} onClick={onClose} />
-                </PopoverFooter>
-              </PopoverContent>
-            </Popover>
-          </>
-        )}
-      </Flex>
-    </Flex>
+        {isOpen ? (
+          <Box pb={4} display={{ md: "none" }}>
+            <Stack as={"nav"} spacing={4}>
+              {Links.map((link) => (
+                <NavLink key={link}>{link}</NavLink>
+              ))}
+            </Stack>
+          </Box>
+        ) : null}
+      </Box>
+    </>
   );
-};
-
-export default Nav;
+}
