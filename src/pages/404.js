28e514cb248c11c379fd3009cd2cf7e585617ab7
Glenn Tippett <glenn.tippett@gmail.com>
2021-12-28 21:41:48 +1100

    404 img

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,9 +1,10 @@
-import { Flex, Heading } from "@chakra-ui/react";
+import { Flex, Heading, Image } from "@chakra-ui/react";
 
 export const notFound = () => {
   return (
-    <Flex minH="60vh" justify="center" align="center">
-      <Heading as="h1">Not Found </Heading>
+    <Flex minH="60vh" justify="center" align="center" direction='column'>
+      <Heading as="h1" mb={4}>404 Not Found </Heading>
+      <Image src={process.env.PUBLIC_URL + "404.gif"} minH='10rem' />
     </Flex>
   );
 };
